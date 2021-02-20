c236cc05516198649cbbcb688c9fc83baf136e2c
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-20 21:58:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names = []
   names.each do |name|
   names.push("Hello, my name is #{name}.")
   end
