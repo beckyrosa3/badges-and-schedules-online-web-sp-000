67c8cfecca86e9564c058989f69307ec5cd7aa91
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-20 21:57:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,14 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(names)
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each do |name|
+  names.push("Hello, my name is #{name}.")
+  end
+  return names
+end
+
 def batch_badge_creator(name)
   name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts "Hello, my name is #{name}."
